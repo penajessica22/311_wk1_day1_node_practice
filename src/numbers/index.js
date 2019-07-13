0050825ede5291f9fc44c0e6f386d6a6818dc1ea
Jessica Pena <penajessica123@yahoo.com>
2019-07-13 13:02:29 -0500

    finished the strings assignment

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,28 +1,28 @@
-const isEven = (num) => {
+const isEven = num => {
   // write code for numbers.isEven
- if (num % 2 === 0){
-   return true;
- } else if(num2 % 3 === 1){
-   return false;
- }
-}
-
-
-// const sum = (arr) => 
-function sumIt(arr) {
-  // write code for numbers.sum
-  return (arr.length === 0) ? 0 : arr[0] + sumIt(arr.slice(1));
-}
-const comboSum = (arr, num) => {
-  // write code for numbers.comboSum
-  let thisSum = sumIt(arr);
-  if (thisSum === num){
+  if (num % 2 === 0) {
     return true;
+  } else if (num2 % 3 === 1) {
+    return false;
   }
+};
+
+function sum(arr) {
+  // write code for numbers.sum
+  return arr.length === 0 ? 0 : arr[0] + sum(arr.slice(1));
 }
 
+// write code for numbers.comboSum
+const comboSum = (arr, num) => {
+  let thisSum = sum(arr);
+  if (thisSum === num) {
+    return true;
+  } else {
+    return false;
+  }
+};
 module.exports = {
   isEven,
   sum,
   comboSum
-}
+};
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,33 @@
-const split = (str, delim) => {
+const split = (str, delimeter) => {
   // write code for strings.split
+  return str.split(delimeter);
+};
 
-}
-
-const pairs = (str) => {
+const pairs = str => {
+  let strArr = Array.from(str);
+  const tempArray = [];
   // write code for strings.pairs
+  for (let index = 0; index < strArr.length; index += 2) {
+    // console.log(index);
+    let element = strArr[index];
+    let chunks = [element, strArr[index + 1]];
+    let joinedChunks = chunks.join("");
+    tempArray.push(joinedChunks);
+  }
+  return tempArray;
+};
+pairs("abcdefghijklmnop");
 
-}
-
-const reverse = (str) => {
+const reverse = str => {
   // write code for strings.reverse
-
-}
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
+};
 
 module.exports = {
   split,
   pairs,
   reverse
-}
+};
